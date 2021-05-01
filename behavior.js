13330f92a54cc773ca2f2b771074c4df145c1c1c
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-05-01 15:18:51 +0100

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -1,1 +1,18 @@
-sfe
+document.addEventListener("DOMContentLoaded", function(event) {
+    alert('Hello! Mightysho welcome you..!');
+});
+document.getElementById("smart_thumbnail");
+var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.addEventListener("click", function() {
+    alert("Mightysho saw you click!")
+});
+if (thumbnailElement.className = "small") {
+    thumbnailElement.addEventListener("click", function() {
+        thumbnailElement.className = "";
+    });
+} else {
+    thumbnailElement.addEventListener("click", function() {
+        thumbnailElement.className = "small";
+    });
+}
+});
