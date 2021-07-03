65f6448d57c1e8ad49f267845018d38969439c0b
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-07-03 15:13:36 +0100

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -3,3 +3,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     alert('Update is currently going on the website, kindly check back later. Thanks! _ Micheal Shokunbi');
 //     alert('Kindly click on the small image on my profile.. See the magic.. Click again.. Ignore PopUp after done.. It will keep coming.');
 });
+document.addEventListener("DOMContentLoaded", function(event) {
+
+var thumbnailElement = document.getElementById("smart_thumbnail");
+thumbnailElement.addEventListener("click", function() {
+if (thumbnailElement.className == "") {
+        thumbnailElement.className = "small";
+}else {
+        thumbnailElement.className = "";
+    }
+  });
+});
