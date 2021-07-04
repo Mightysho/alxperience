6a7beb83a6a6dfcf66712aea65a5fd0939cb878b
Mightysho <82241991+Mightysho@users.noreply.github.com>
2021-07-04 01:33:39 +0100

    Update behavior.js

--- behavior.js
+++ behavior.js
@@ -14,3 +14,33 @@ if (thumbnailElement.className == "") {
     }
   });
 });
+
+<div class="nav-user-info">
+                                    <h5 class="mb-0 text-white nav-user-name">Micheal Shokunbi </h5>
+                                    <span class="status"></span><span class="ml-2">Available</span>
+                                </div>
+
+      <script language="JavaScript">
+var myDate = new Date();
+  
+  
+/* hour is before noon */
+if ( myDate.getHours() < 12 ) 
+{
+    document.write("Good Morning!");
+}
+else  /* Hour is from noon to 5pm (actually to 5:59 pm) */
+if ( myDate.getHours() >= 12 && myDate.getHours() <= 17 )
+{
+    document.write("Good Afternoon!");
+}
+else  /* the hour is after 5pm, so it is between 6pm and midnight */
+if ( myDate.getHours() > 17 && myDate.getHours() <= 24 )
+{
+    document.write("Good Evening!");
+}
+else  /* the hour is not between 0 and 24, so something is wrong */
+{
+    document.write("I'm not sure what time it is!");
+}
+  </script>
